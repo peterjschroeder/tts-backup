bc9e4f006eb9a3577e90f4ed71466ba2abbae7ad
peter <peterjschroeder@gmail.com>
2026-02-14 20:15:23 -0500

    Undo fix.

--- src/tts_tools/libtts.py
+++ src/tts_tools/libtts.py
@@ -56,8 +56,7 @@ def seekURL(dic, trail=[]):
             for elem in v:
                 if not isinstance(elem, dict):
                     continue
-                # Fix for Steam CDN and old pastebin urls
-                yield from seekURL(elem.replace('http://cloud-3.steamusercontent.com/', 'https://steamusercontent-a.akamaihd.net/').replace('https://pastebin.com/raw.php?i=', 'https://pastebin.com/raw/'), newtrail)
+                yield from seekURL(elem, newtrail)
 
         elif k.lower().endswith("url"):
             # We don’t want tablet URLs.
